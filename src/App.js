303ebc014a33d9f4c7e1fb6ef57d6ae1c769e320
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2023-01-28 22:12:21 -0300

    Added products cards to category, with hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductCard from './components/Card/ProductCard';
+import CategoryComponent from './components/CategoryComponent/CategoryComponent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         actionButton={'Comprar'}
         price={'$1000'}
       />
+      <CategoryComponent />
     </div>
   );
 }
---
+++ src/components/CategoryComponent/CategoryComponent.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import './categoryComponent.css';
+
+const CategoryComponent = () => {
+  return (
+    <div className="d-flex mt-5">
+      <div className="categoryWrapper">
+        <h4>Home and outdoor</h4>
+        <button>Source now</button>
+      </div>
+      <div className="cardWrapper">
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+        <div className="card-container">
+          <div className="d-flex flex-column">
+            <h5>Soft Chair</h5>
+            <p className="m-0">From</p>
+            <p>19USD</p>
+          </div>
+          <img
+            src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/130/818/products/1-41-52cd30a55126d4d96615852634820386-640-0.jpg"
+            alt="sillon"
+            className="imgCategoryProd"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryComponent;
